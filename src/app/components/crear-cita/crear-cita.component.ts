c3dab7eaca0bfed35119311b8b9b6b5c46ac4f6c
juanjoml <telefilme@gmail.com>
2023-01-26 20:48:27 +0100

    Se agrega el proyecto

---
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-cita',
+  templateUrl: './crear-cita.component.html',
+  styleUrls: ['./crear-cita.component.css']
+})
+export class CrearCitaComponent implements OnInit {
+  nombre = '';
+  fecha = '';
+  hora = '';
+  sintomas = '';
+  formularioIncorrecto = false;
+  @Output()
+  nuevaCita = new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregarCita(){
+    if(this.nombre == '' || this.fecha == '' || this.hora == '' ||this.sintomas == ''){
+      this.formularioIncorrecto = true;
+      return;
+    }
+    this.formularioIncorrecto = false;
+    const CITA = {
+      nombre: this.nombre,
+      fecha: this.fecha,
+      hora: this.hora,
+      sintomas: this.sintomas
+    }
+    console.log(CITA);
+    this.nuevaCita.emit(CITA);
+    this.resetCampos();
+  }
+
+  resetCampos(){
+    this.nombre='';
+    this.nombre='';
+    this.hora='';
+    this.sintomas='';
+  }
+}
